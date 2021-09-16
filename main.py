a0f9321c4d6ba64b29c9134e5b29b0eff9b1e65b
이민호 <minho.lee0716@gmail.com>
2021-09-16 11:25:44 +0900

    Modify: Request Test

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+import uvicorn
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -12,3 +21,58 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/json")
+async def json_test(req: Request, body: Item):
+#    for element in dir(req):
+#        if element[0] == "_":
+#            continue
+#        print(element)
+
+    print('client = ', req.client)
+    print('cookies = ', req.cookies)
+    print('form = ', req.form)
+    print('get = ', req.get)
+    print('headers = ', req.headers)
+    print('json = ', req.json)
+    print('keys = ', req.keys)
+    print('method = ', req.method)
+    print('path_params = ', req.path_params)
+#    print('query_params = ', req.query_params)
+    print('receive = ', req.receive)
+    print('scope = ', req.scope)
+#    print('session = ', req.session)
+    print('url = ', req.url)
+    print('values = ', req.values)
+
+#    qp = req.query_params
+#    print(qp)
+#    print(type(qp))
+#    print(dir(qp))
+#    print(qp.values())
+#    print(qp._dict)
+    """
+    req.query_params._dict = {"q": "123", "c": "hi"}
+    """
+
+#    print(req.values())
+#    print(dir(req.values))
+
+    print(body)
+
+    return {"message": "success"}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        #reload_delay=3.00,
+        log_level="info"
+    )
